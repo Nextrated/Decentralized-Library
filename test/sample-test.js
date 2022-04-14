35cf1cc07753e19c612a8e698194b6b4591a1a66
Denedo Pearl <pearliboy1@gmail.com>
2022-04-14 05:41:54 +0100

    added a few tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,58 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+require("@nomiclabs/hardhat-waffle");
+ 
+ describe("Decentralized Library", function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  beforeEach(async function () {
+    const owner = await ethers.getSigners();
+    const DecentralizedLibrary = await ethers.getContractFactory("DecentralizedLibrary");
+    const decentralizedLibrary = await DecentralizedLibrary.deploy();
+    
   });
+
+  //fileUpload function broken into bits
+
+  it("checks to confirm if fileExists is false", async function () {
+
+    const fileExists = boolean = false;
+    expect (fileExists).to.equal(false);
+  });
+
+  it("checks to confirm if uploadType is private", async function () {
+    
+    const uploadType = uint = (1);
+      expect(uploadType).to.equal(1);  
+});
+
+  it("checks to confirm if uploadType is public", async function () {
+    
+  const uploadType = uint = (0);
+    expect(uploadType).to.equal(0);  
+    
+});
+
+it("checks to confirm if fileExists has been reset to true before file fileUpload", async function () {
+
+  const fileExists = boolean = true;
+  expect(fileExists).to.equal(true);
+
+});
+
+it("checks to confirm if fileUpload is successful", async function () {
+
+  const fileUpload = string = ("_ipfsCID, _fileName, block.timestamp, msg.sender");
+  expect(fileUpload).to.equal("_ipfsCID, _fileName, block.timestamp, msg.sender");
+  
+});
+
+it("checks to get the size of the array", async function () {
+
+  const getSize = uint = ("keys.length");
+  expect(getSize).to.equal("keys.length");
+  
+});
+
 });
