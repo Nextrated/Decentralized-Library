44f46cbe2fff0023aa5b1bf9a570d83ab41d78be
Phyf3 <phade075@gmail.com>
2022-04-14 12:42:37 +0100

    removed: redundant parameters

--- contracts/DecentralizedLibrary.sol
+++ contracts/DecentralizedLibrary.sol
@@ -17,7 +17,6 @@ contract DecentralizedLibrary {
 
     mapping (address => mapping(string => FileDetail)) public sharedCollection;
     mapping(address => string[]) private sKey;
-    address[] public sharedAccess;
 
 
     mapping (string => bool) public fileExists;
@@ -38,12 +37,6 @@ contract DecentralizedLibrary {
     /// @notice Makes sure the current address is the only owner -> for private files functions
     constructor(){
         owner = msg.sender;
-    }
-
-    /// @notice Makes sure the current address is the only owner
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only current address can do this");
-        _;
     }
 
 
@@ -241,7 +234,6 @@ contract DecentralizedLibrary {
         FileDetail memory fileDetails = FileDetail(_ipfsCID, _filename , _timeUploaded, _fileOwner);
         sharedCollection[_to][_fileName] = fileDetails;
         sKey[_to].push(_fileName);
-        sharedAccess.push(_to);
     }
 
 
